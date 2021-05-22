6dbf05d67179b8ad7470f11ac5a5c35fd3345967
kinr-jay <cjacob22@gmail.com>
2021-05-21 23:02:10 -0600

    boilerplate, models, controllers with stock msg responses

---
+++ controllers/gardens.js
@@ -0,0 +1,70 @@
+const mongoose = require("../db/connection.js")
+const db = mongoose.connection
+const router = require("express").Router()
+const Garden = require("../models/garden.js")
+const Plant = require("../models/Plant.js")
+
+// Create a new Garden
+router.post("/", async (req, res) => {
+  try {
+    res.send("/gardens route hit with POST request")
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// Validate user, Find Garden and Plants
+router.get("/:email", async (req, res) => {
+  try {
+    res.send("/gardens/:email route hit with GET request")
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// Find a Garden by ObjectId
+router.get("/:id", async (req, res) => {
+  try {
+    res.send("/gardens/:id route hit with GET request")
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// Find all Plants in Garden by Garden's ObjectId
+router.get("/:id/plants", async (req, res) => {
+  try {
+    res.send("/gardens/plants route hit with GET request")
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// Add a single Plant to a Garden by the Garden's ObjectId
+router.post("/:id/plants", async (req, res) => {
+  try {
+    res.send("/gardens/plants route hit with POST request")
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// Update a single Plant by ObjectId in a Garden by ObjectId
+router.put("/:id/plants/:id", async (req, res) => {
+  try {
+    res.send("/gardens/plants/:id route hit with GET request")
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// Delete a single Plant by ObjectId in a Garden by ObjectId
+router.delete("/:id/plants/:id", async (req, res) => {
+  try {
+    res.send("/gardens/plants/:id route hit with POST request")
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,28 @@
+// ENV variables
+require("dotenv").config()
+const { PORT = 4500, NODE_ENV = "development" } = process.env
+
+// CORS
+const cors = require("cors")
+const corsOptions = require("./config/cors.js")
+
+// Express
+const express = require("express")
+const app = express()
+
+// Logger
+const morgan = require("morgan")
+
+// Middleware
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(morgan("dev"))
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("<h1>Heeeyy everybody, it's Alex Merced.</h1>")
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
